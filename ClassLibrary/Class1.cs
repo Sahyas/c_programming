5eef0e96e2ddc5e4089e664a5ca327bdcf797a59
236707 Szymon Zakrzewski <236707@edu.p.lodz.pl>
2022-03-23 23:31:45 +0100

    proste dodawanie

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public int Add(int x, int y)
+        {
+            int result;
+            int a = 5;
+            int b = 10;
+            result = a + b;
+            Console.WriteLine(result);
+        }
+    }
+}
