23ff33da38dd6d5b40df0baeb1ea00ba3f3a4322
skilluuu <skonieczny.piotr01@gmail.com>
2022-03-24 12:44:59 +0100

    Simple calkulator - few more functions added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,12 +6,28 @@ namespace ClassLibrary
     {
         public static void Main()
         {
-            int result;
-            int a = 5;
-            int b = 10;
-            int c = 3;
-            result = (a + b)/c;
-            Console.WriteLine(result);
+            Console.WriteLine("Podaj 3 liczby: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Class1 klasa = new Class1();
+            double result0 = klasa.Add(a,b,c);
+            double result1 = klasa.Sub(a,b,c); 
+            double result2 = klasa.Mult(a,b,c);
+            double result3 = klasa.Div(a,b,c);
+            Console.WriteLine("Dodawanie:");
+            Console.WriteLine(result0);
+            Console.WriteLine("Odejmowanie:");
+            Console.WriteLine(result1);
+            Console.WriteLine("Mnożenie:");
+            Console.WriteLine(result2);
+            Console.WriteLine("Dzielenie:");
+            Console.WriteLine(result3);
         }
+
+        public double Add(double a, double b, double c) { return a + b + c; }  
+        public double Sub(double a, double b, double c) { return a - b - c; }   
+        public double Mult(double a, double b, double c) { return a * b * c; }
+        public double Div(double a, double b, double c) { return a / b / c; }
     }
 }
