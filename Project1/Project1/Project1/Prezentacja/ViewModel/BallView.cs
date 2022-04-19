ef6737e24785637d6d579bc61dda0f4d16108b58
236707 Szymon Zakrzewski <236707@edu.p.lodz.pl>
2022-04-19 02:38:15 +0200

    prosty widok aplikacji xaml - sprawdzenie poprawności działanaia projektu WPF

---
+++ Project1/Project1/Project1/Prezentacja/ViewModel/BallView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    using Model;
+    public class BallView : INotifyPropertyChanged
+    { 
+
+    private BallView model = new BallView();
+
+        public double wartoscPromienia 
+        {
+            get {
+                return model.wartoscPromienia; 
+            }
+            set { 
+                model.wartoscPromienia = value;
+                onPropertyChanged(nameof(wartoscPromienia));
+            }
+        }
+    private void onPropertyChanged(string nazwa)
+        {
+            if(PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(nazwa));
+        }
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+}
+
+}
