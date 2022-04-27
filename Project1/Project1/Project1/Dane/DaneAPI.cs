9ff1b6ba210697f47c5abbcaa7ce4d0bca0dc33b
236707 Szymon Zakrzewski <236707@edu.p.lodz.pl>
2022-04-27 20:53:48 +0200

    dodanie API logiki oraz danych

---
+++ Project1/Project1/Project1/Dane/DaneAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dane
+{
+    public abstract class DaneAPI
+    {
+        public static DaneAPI CreateDataAPI()
+        {
+            return new DaneBall();
+        }
+
+        private class DaneBall : DaneAPI
+        {
+            public DaneBall()
+            {
+                //do sth in future
+            }
+                
+
+        }
+    }
+}
--- Project1/Project1/Project1/Logika/LogicAPI.cs
+++ Project1/Project1/Project1/Logika/LogicAPI.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Dane;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Logika
 {
-    internal class LogicAPI
+    internal abstract class LogicAPI
     {
+        public abstract void addBalls(int BallsNumber);
+
+        public abstract void start();
+
+        public abstract List<Ball> getBalls();
+
+        public static LogicAPI Create(DaneAPI data = default(DaneAPI))
+        {
+            return new BussinessLogic(
+                data == null ? DaneAPI.CreateDataAPI() : data);
+        }
+
+        private class BussinessLogic : LogicAPI
+        {
+            private DaneAPI daneAPI;
+            private Task updatePosition;
+            private Board Board;
+
+            public BussinessLogic(DaneAPI daneAPI)
+            {
+                this.daneAPI = daneAPI;
+                Board = new Board(800);
+            }
+
+            public override void addBalls(int BallsNumber)
+            {
+                Board.addBalls(BallsNumber);
+            }
+
+            public override List<Ball> getBalls()
+            {
+                return Board.balls;
+            }
+
+            public override void start()
+            {
+                updatePosition = Task.Run(Board.MoveBallsConstantly);
+            }
+        }
+
     }
 }
