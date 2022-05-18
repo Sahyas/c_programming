1370bc41620c62b575e44e1d98f0bc46cb07ce1c
236707 Szymon Zakrzewski <236707@edu.p.lodz.pl>
2022-05-19 00:39:15 +0200

    zmiana warstwy logic. uaktualnienie tworzenia się obiektów kul. Dodanie kolizji pomiędzy kulami

---
+++ Project1/Project1/Project1/Logika/CollisionController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using System.Numerics;
+
+namespace Logika
+{
+    public class CollisionControler
+    {
+        private int mass;
+        private int radious;
+        private Vector2 position;
+        private Vector2 velocity;
+
+        public CollisionControler(double poitionX, double poitionY, double speedX, double speedY, int radious, int mass)
+        {
+            this.velocity = new Vector2(speedX, speedY);
+            this.position = new Vector2(poitionX, poitionY);
+            this.radious = radious;
+            this.mass = mass;
+        }
+
+        public bool IsCollision(double otherX, double otherY, double otherRadius, bool mode)
+        {
+            double currentX;
+            double currentY;
+            if (mode)
+            {
+                currentX = position.X + velocity.X;
+                currentY = position.Y + velocity.Y;
+            }
+            else
+            {
+                currentX = position.X;
+                currentY = position.Y;
+            }
+
+            double distance = Math.Sqrt(Math.Pow(currentX - otherX, 2) + Math.Pow(currentY - otherY, 2));
+
+            if (Math.Abs(distance) <= radious + otherRadius)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsTouchingBoundariesX(int boardSize)
+        {
+            double newX = position.X + velocity.X;
+
+            if ((newX > boardSize && velocity.X > 0) || (newX < 0 && velocity.X < 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTouchingBoundariesY(int boardSize)
+        {
+            double newY = position.Y + velocity.Y;
+            if ((newY > boardSize && velocity.Y > 0) || (newY < 0 && velocity.Y < 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Vector2[] ImpulseSpeed(double otherX, double otherY, double speedX, double speedY, double otherMass)
+        {
+            Vector2 velocityOther = new Vector2(speedX, speedY);
+            Vector2 positionOther = new Vector2(otherX, otherY);
+
+            double fDistance = Math.Sqrt((position.X - positionOther.X) * (position.X - positionOther.X) + (position.Y - positionOther.Y) * (position.Y - positionOther.Y));
+
+            double nx = (positionOther.X - position.X) / fDistance;
+            double ny = (positionOther.Y - position.Y) / fDistance;
+
+            double tx = -ny;
+            double ty = nx;
+
+            // Dot Product Tangent
+            double dpTan1 = velocity.X * tx + velocity.Y * ty;
+            double dpTan2 = velocityOther.X * tx + velocityOther.Y * ty;
+
+            // Dot Product Normal
+            double dpNorm1 = velocity.X * nx + velocity.Y * ny;
+            double dpNorm2 = velocityOther.X * nx + velocityOther.Y * ny;
+
+            // Conservation of momentum in 1D
+            double m1 = (dpNorm1 * (mass - otherMass) + 2.0f * otherMass * dpNorm2) / (mass + otherMass);
+            double m2 = (dpNorm2 * (otherMass - mass) + 2.0f * mass * dpNorm1) / (mass + otherMass);
+
+            return new Vector2[2] { new Vector2(tx * dpTan1 + nx * m1, ty * dpTan1 + ny * m1), new Vector2(tx * dpTan2 + nx * m2, ty * dpTan2 + ny * m2) };
+
+        }
+    }
+}
--- Project1/Project1/Project1/Logika/LogicAPI.cs
+++ Project1/Project1/Project1/Logika/LogicAPI.cs
@@ -1,62 +1,161 @@
-﻿using Dane;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Dane;
 
 namespace Logika
 {
-    public abstract class LogicAPI
+    public abstract class LogicAPI : IObserver<int>, IObservable<int>
     {
-        public abstract void addBalls(int BallsNumber);
+        public abstract void AddBallsAndStart(int BallsAmount);
+        public abstract double getBallPositionX(int ballId);
+        public abstract double getBallPositionY(int ballId);
+        public abstract int getBallRadius(int ballId);
 
-        public abstract void start();
+        public abstract IDisposable Subscribe(IObserver<int> observer);
+        public abstract void OnCompleted();
+        public abstract void OnError(Exception error);
+        public abstract void OnNext(int value);
 
-        public abstract void stop();
 
-        public abstract List<Ball> getBalls();
-
-        public static LogicAPI Create(DaneAPI data = default(DaneAPI))
+        public static LogicAPI CreateLayer(DaneAbstractAPI data = default(DaneAbstractAPI))
         {
-            return new BussinessLogic(
-                data == null ? DaneAPI.CreateDataAPI() : data);
+            return new BusinessLogic(data == null ? DaneAbstractAPI.CreateDataApi() : data);
         }
 
-        private class BussinessLogic : LogicAPI
+        public class BallChaneEventArgs : EventArgs
         {
-            private DaneAPI daneAPI;
-            private Task updatePosition;
-            private Board Board;
+            public int ballId { get; set; }
+        }
 
-            public BussinessLogic(DaneAPI daneAPI)
+        private class BusinessLogic : LogicAPI, IObservable<int>
+        {
+            private readonly DaneAbstractAPI dataAPI;
+            private IDisposable unsubscriber;
+            static object _lock = new object();
+            private IObservable<EventPattern<BallChaneEventArgs>> eventObservable = null;
+            public event EventHandler<BallChaneEventArgs> BallChanged;
+
+            public BusinessLogic(DaneAbstractAPI dataAPI)
             {
-                this.daneAPI = daneAPI;
-                Board = new Board(562);
+                eventObservable = Observable.FromEventPattern<BallChaneEventArgs>(this, "BallChanged");
+                this.dataAPI = dataAPI;
+                Subscribe(dataAPI);
             }
 
-            public override void addBalls(int BallsNumber)
+            public override double getBallPositionX(int ballId)
             {
-                Board.addBalls(BallsNumber);
+                return this.dataAPI.getBallPositionX(ballId);
             }
 
-            public override List<Ball> getBalls()
+            public override double getBallPositionY(int ballId)
             {
-                return Board.balls;
+                return this.dataAPI.getBallPositionY(ballId);
             }
 
-            public override void start()
+            public override int getBallRadius(int ballId)
             {
-                if (Board.balls.Count > 0)
+                return this.dataAPI.getBallRadius(ballId);
+            }
+
+
+            public override void AddBallsAndStart(int BallsAmount)
+            {
+                dataAPI.createBalls(BallsAmount);
+            }
+
+            #region observer
+
+            public virtual void Subscribe(IObservable<int> provider)
+            {
+                if (provider != null)
+                    unsubscriber = provider.Subscribe(this);
+            }
+
+            public override void OnNext(int value)
+            {
+                Monitor.Enter(_lock);
+                try
                 {
-                    updatePosition = Task.Run(Board.MoveBallsConstantly);
+                    CollisionControler collisionControler = new CollisionControler(dataAPI.getBallPositionX(value), dataAPI.getBallPositionY(value), dataAPI.getBallSpeedX(value), dataAPI.getBallSpeedY(value), dataAPI.getBallRadius(value), 10);
+
+                    for (int i = 1; i <= dataAPI.getBallsAmount(); i++)
+                    {
+                        if (value != i)
+                        {
+                            double otherBallX = dataAPI.getBallPositionX(i);
+                            double otherBallY = dataAPI.getBallPositionY(i);
+                            double otherBallSpeedX = dataAPI.getBallSpeedX(i);
+                            double otherBallSpeedY = dataAPI.getBallSpeedY(i);
+                            int otherBallRadius = dataAPI.getBallRadius(i);
+                            double otherBallMass = dataAPI.getBallMass(i);
+
+                            if (collisionControler.IsCollision(otherBallX + otherBallSpeedX, otherBallY + otherBallSpeedY, otherBallRadius, true))
+                            {
+                                if (!collisionControler.IsCollision(otherBallX, otherBallY, otherBallRadius, false))
+                                {
+                                    System.Diagnostics.Trace.WriteLine("Ball " + value + " hit ball " + i);
+
+                                    Vector2[] newVelocity = collisionControler.ImpulseSpeed(otherBallX, otherBallY, otherBallSpeedX, otherBallSpeedY, otherBallMass);
+
+                                    dataAPI.setBallSpeed(value, newVelocity[0].X, newVelocity[0].Y);
+                                    dataAPI.setBallSpeed(i, newVelocity[1].Y, newVelocity[1].Y);
+                                }
+                            }
+                        }
+                    }
+
+                    int boardSize = dataAPI.getBoardSize();
+
+                    if (collisionControler.IsTouchingBoundariesX(boardSize))
+                    {
+                        dataAPI.setBallSpeed(value, -dataAPI.getBallSpeedX(value), dataAPI.getBallSpeedY(value));
+                    }
+
+                    if (collisionControler.IsTouchingBoundariesY(boardSize))
+                    {
+                        dataAPI.setBallSpeed(value, dataAPI.getBallSpeedX(value), -dataAPI.getBallSpeedY(value));
+                    }
+                    BallChanged?.Invoke(this, new BallChaneEventArgs() { ballId = value });
+                }
+                catch (SynchronizationLockException exception)
+                {
+                    throw new Exception("Checking collision synchronization lock not working", exception);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
                 }
             }
 
-            public override void stop()
+            public override void OnCompleted()
             {
-                Board.balls.Clear();
+                Unsubscribe();
             }
-        }
 
+            public override void OnError(Exception error)
+            {
+                throw error;
+            }
+
+            public virtual void Unsubscribe()
+            {
+                unsubscriber.Dispose();
+            }
+
+            #endregion
+
+            #region observable
+            public override IDisposable Subscribe(IObserver<int> observer)
+            {
+                return eventObservable.Subscribe(x => observer.OnNext(x.EventArgs.ballId), ex => observer.OnError(ex), () => observer.OnCompleted());
+            }
+            #endregion
+
+        }
     }
 }
